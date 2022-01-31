844c0a9b85fec3cafc95e431e2b52eb50fa289f9
jysohn0825 <jysohn0825@naver.com>
2022-01-31 17:29:46 +0900

    add : adding swagger ui

--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"github.com/WalkingMileageService/BMS/router"
+	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
+	// Side Effect import for auto-generated swagger documentation
+	//_ "medium_go_fiber_swagger/docs"
 )
 
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @contact.name API Support
+// @contact.email youremail@provider.com
+// @host localhost:3000
+// @BasePath /
 func main() {
 	app := fiber.New()
+	app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
+		URL:         "/swagger/doc.json",
+		DeepLinking: false,
+	}))
 	router.SetupRoutes(app)
 	app.Listen(":3000")
-
 }
