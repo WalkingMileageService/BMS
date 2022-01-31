6614f1887f143d14248e2565cb06058c52995dbc
jysohn0825 <jysohn0825@naver.com>
2022-01-31 17:39:15 +0900

    fix : change swagger setting

--- board/board_controller.go
+++ board/board_controller.go
@@ -2,6 +2,18 @@ package board
 
 import "github.com/gofiber/fiber/v2"
 
+// ShowAccount godoc
+// @Summary      Show an account
+// @Description  get string by ID
+// @Tags         accounts
+// @Accept       json
+// @Produce      json
+// @Param        id   path      int  true  "Account ID"
+// @Success      200  {object}  model.Account
+// @Failure      400  {object}  httputil.HTTPError
+// @Failure      404  {object}  httputil.HTTPError
+// @Failure      500  {object}  httputil.HTTPError
+// @Router       /accounts/{id} [get]
 func CreateBoard(c *fiber.Ctx) error {
 	return nil
 }
